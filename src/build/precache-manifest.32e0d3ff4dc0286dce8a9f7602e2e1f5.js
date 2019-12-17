5008fd653451db10ebd67b7aaceb5112b3761b5e
freddykibz <fred@kibzagency.com>
2019-12-17 11:10:42 +0300

    First Commit

---
+++ src/build/precache-manifest.32e0d3ff4dc0286dce8a9f7602e2e1f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7496357198da2c4dd95c624b3096dba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de632336f0b5ac12e0e3",
+    "url": "/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "2bd9f7a4fb0418fc76a2",
+    "url": "/static/css/main.f531e0d8.chunk.css"
+  },
+  {
+    "revision": "de632336f0b5ac12e0e3",
+    "url": "/static/js/2.45a0956f.chunk.js"
+  },
+  {
+    "revision": "2bd9f7a4fb0418fc76a2",
+    "url": "/static/js/main.d32612cb.chunk.js"
+  },
+  {
+    "revision": "35a7acf4fb928453b799",
+    "url": "/static/js/runtime-main.8727d782.js"
+  }
+]);
